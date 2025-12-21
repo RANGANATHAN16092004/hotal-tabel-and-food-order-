5a193124af84618900d63a38498ef784ba9ce304
RANGANATHAN16092004 <ranganathanvenkat60@gmail.com>
2025-12-21 13:46:48 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -45,10 +45,15 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+app.get("/", (req, res) => {
+  res.send("Backend running on Vercel 🚀");
 });
 
+// ❌ DO NOT use app.listen() in Vercel
+export default app;
 
